407b8381552f8676bb1bafe1548b72f7c46486ac
Daniel Selans <daniel.selans@gmail.com>
2020-08-06 16:16:44 -0700

    remove debug comments

--- main.go
+++ main.go
@@ -18,24 +18,18 @@ func main() {
 	switch cmd {
 	// Read
 	case "read message rabbit":
-		//logrus.Fatal("read rabbit, ", opts)
 		err = rabbitmq.Read(opts)
 	case "read message kafka":
-		//logrus.Fatalf("read kafka, ", opts)
 		err = kafka.Read(opts)
 	case "read message gcp-pubsub":
-		//logrus.Fatalf("read gcp-pubsub, ", opts)
 		err = gcppubsub.Read(opts)
 
 	// Write
 	case "write message rabbit":
-		//logrus.Fatalf("write rabbit, ", opts)
 		err = rabbitmq.Write(opts)
 	case "write message kafka":
-		//logrus.Fatalf("write kafka, ", opts)
 		err = kafka.Write(opts)
 	case "write message gcp-pubsub":
-		//logrus.Fatalf("write gcp-pubsub, ", opts)
 		err = gcppubsub.Write(opts)
 
 	default:
