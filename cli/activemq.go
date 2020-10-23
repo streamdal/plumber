0cf0794904705d7749a6348aa8cb92d4d698b7d7
Mark Gregan <mark@batch.sh>
2020-10-23 14:14:03 -0400

    cleaning up activemq options

--- cli/activemq.go
+++ cli/activemq.go
@@ -1,8 +1,6 @@
 package cli
 
 import (
-	"time"
-
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -11,40 +9,22 @@ type ActiveMqOptions struct {
 	Address  string
 	Topic    string
 	Queue    string
-	Timeout  time.Duration
 	ClientId string
-	QoSLevel int
 
 	// Read
-	ReadFollow  bool
-	ReadTimeout time.Duration
-
-	// Write
-	WriteTimeout time.Duration
+	ReadFollow bool
 }
 
 func HandleActiveMqFlags(readCmd, writeCmd *kingpin.CmdClause, opts *Options) {
-	rc := readCmd.Command("activemq", "ActiveMq STOMP")
-
+	rc := readCmd.Command("activemq", "ActiveMQ STOMP")
 	addSharedActiveMqFlags(rc, opts)
-	addReadActiveMqFlags(rc, opts)
 
-	wc := writeCmd.Command("activemq", "ActiveMq STOMP")
-
+	wc := writeCmd.Command("activemq", "ActiveMQ STOMP")
 	addSharedActiveMqFlags(wc, opts)
-	addWriteActiveMqFlags(wc, opts)
 }
 
 func addSharedActiveMqFlags(cmd *kingpin.CmdClause, opts *Options) {
 	cmd.Flag("address", "Destination host address").Default("tcp://localhost:61613").StringVar(&opts.ActiveMq.Address)
 	cmd.Flag("topic", "Topic to read message(s) from").StringVar(&opts.ActiveMq.Topic)
 	cmd.Flag("queue", "Topic to read message(s) from").StringVar(&opts.ActiveMq.Queue)
-	cmd.Flag("timeout", "Connect timeout").DurationVar(&opts.ActiveMq.Timeout)
-}
-
-func addReadActiveMqFlags(cmd *kingpin.CmdClause, opts *Options) {
-
-}
-
-func addWriteActiveMqFlags(cmd *kingpin.CmdClause, opts *Options) {
 }
