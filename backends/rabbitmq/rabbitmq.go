fa45be0de95092d818e971e252f8fb7be2858e1d
Daniel Selans <daniel.selans@gmail.com>
2021-02-04 10:33:46 -0800

    fix queue-declare

--- backends/rabbitmq/rabbitmq.go
+++ backends/rabbitmq/rabbitmq.go
@@ -32,6 +32,7 @@ func New(opts *cli.Options, md *desc.MessageDescriptor) (*RabbitMQ, error) {
 		RoutingKey:     opts.Rabbit.RoutingKey,
 		QueueExclusive: opts.Rabbit.ReadQueueExclusive,
 		QueueDurable:   opts.Rabbit.ReadQueueDurable,
+		QueueDeclare:   opts.Rabbit.ReadQueueDeclare,
 		AutoAck:        opts.Rabbit.ReadAutoAck,
 		ConsumerTag:    opts.Rabbit.ReadConsumerTag,
 		AppID:          opts.Rabbit.WriteAppID,
