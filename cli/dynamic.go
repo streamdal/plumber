2cc1462b52e67705bfb5aba4e6c93a094d8e57f4
Mark Gregan <mark@batch.sh>
2021-05-25 17:21:45 -0400

    Adding missing commands in cli/dynamic.go

--- cli/dynamic.go
+++ cli/dynamic.go
@@ -8,17 +8,44 @@ type DynamicOptions struct {
 }
 
 func HandleDynamicFlags(dynamicCmd *kingpin.CmdClause, opts *Options) {
+	amqCmd := dynamicCmd.Command("activemq", "ActiveMQ as Replay Destination")
+	addSharedActiveMqFlags(amqCmd, opts)
+
+	snsCmd := dynamicCmd.Command("aws-sns", "AWS SNS as Replay Destination")
+	addWriteAWSSNSFlags(snsCmd, opts)
+
+	sqsCmd := dynamicCmd.Command("aws-sqs", "AWS SQS as Replay Destination")
+	addSharedAWSSQSFlags(sqsCmd, opts)
+	addWriteAWSSQSFlags(sqsCmd, opts)
+
+	azureCmd := dynamicCmd.Command("azure", "Azure Service Bus as Replay Destination")
+	addSharedAzureFlags(azureCmd, opts)
+
+	azureEventHubCmd := dynamicCmd.Command("azure-eventhub", "Azure Event Hub as Replay Destination")
+	addSharedAzureEventhubFlags(azureEventHubCmd, opts)
+	addWriteAzureEventhubFlags(azureEventHubCmd, opts)
+
+	gcpCmd := dynamicCmd.Command("gcp-pubsub", "GCP PubSub as Replay Destination")
+	addSharedGCPPubSubFlags(gcpCmd, opts)
+	addWriteGCPPubSubFlags(gcpCmd, opts)
+
 	kafkaCmd := dynamicCmd.Command("kafka", "Kafka as Replay Destination")
 	addSharedKafkaFlags(kafkaCmd, opts)
 	addWriteKafkaFlags(kafkaCmd, opts)
 
-	rabbitCmd := dynamicCmd.Command("rabbit", "RabbitMQ as Replay Destination")
-	addSharedRabbitFlags(rabbitCmd, opts)
-	addWriteKafkaFlags(rabbitCmd, opts)
-
 	mqttCmd := dynamicCmd.Command("mqtt", "MQTT as Replay Destination")
 	addSharedMQTTFlags(mqttCmd, opts)
 	addWriteMQTTFlags(mqttCmd, opts)
+
+	natsCmd := dynamicCmd.Command("nats", "NATS as Replay Destination")
+	addSharedNatsFlags(natsCmd, opts)
+
+	natsStreamingCmd := dynamicCmd.Command("nats-streaming", "NATS Streaming as Replay Destination")
+	addSharedNatsStreamingFlags(natsStreamingCmd, opts)
+
+	rabbitCmd := dynamicCmd.Command("rabbit", "RabbitMQ as Replay Destination")
+	addSharedRabbitFlags(rabbitCmd, opts)
+	addWriteRabbitFlags(rabbitCmd, opts)
 
 	rPubSubCmd := dynamicCmd.Command("redis-pubsub", "Redis PubSub as Replay Destination")
 	addSharedRedisPubSubFlags(rPubSubCmd, opts)
@@ -27,9 +54,4 @@ func HandleDynamicFlags(dynamicCmd *kingpin.CmdClause, opts *Options) {
 	addSharedRedisStreamsFlags(rStreams, opts)
 	addWriteRedisStreamsFlags(rStreams, opts)
 
-	natsCmd := dynamicCmd.Command("nats", "NATS as Replay Destination")
-	addSharedNatsFlags(natsCmd, opts)
-
-	natsStreamingCmd := dynamicCmd.Command("nats-streaming", "NATS Streaming as Replay Destination")
-	addSharedNatsFlags(natsStreamingCmd, opts)
 }
