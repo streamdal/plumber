250513010cf0fb61edfb323cd2467d16ae7155fe
Mark Gregan <mark@batch.sh>
2021-08-11 12:37:47 -0400

    removing debug return

--- plumber/server.go
+++ plumber/server.go
@@ -35,9 +35,6 @@ func (p *Plumber) RunServer() error {
 }
 
 func (p *Plumber) startEtcd() error {
-	// TODO: remove
-	return nil
-
 	e, err := etcd.New(p.Config.Options.Server)
 	if err != nil {
 		return errors.Wrap(err, "unable to instantiate etcd")
