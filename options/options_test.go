1bd436a15f7e7b0a25e89efb7d21d44981f13fd2
Mark Gregan <mark@batch.sh>
2021-08-17 18:25:26 -0400

    Fixing options_test.go

--- options/options_test.go
+++ options/options_test.go
@@ -41,9 +41,9 @@ func TestHandleRabbitFlags_read(t *testing.T) {
 	g.Expect(opts.Rabbit.ReadQueueDeclare).To(BeFalse())
 	g.Expect(opts.Rabbit.ReadAutoAck).To(BeFalse())
 	g.Expect(opts.Rabbit.ReadConsumerTag).To(Equal("plumber_123"))
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleRabbitFlags_relay(t *testing.T) {
@@ -77,12 +77,12 @@ func TestHandleRabbitFlags_relay(t *testing.T) {
 
 	g.Expect(err).ToNot(HaveOccurred())
 	g.Expect(cmd).To(Equal("relay rabbit"))
-	g.Expect(opts.RelayType).To(Equal("rabbit"))
-	g.Expect(opts.RelayGRPCDisableTLS).To(BeTrue())
-	g.Expect(opts.RelayGRPCTimeout).To(Equal(time.Second * 4))
-	g.Expect(opts.RelayToken).To(Equal("8EDB98ED-0D85-4CFD-BE24-8B1E00A9F7C3"))
-	g.Expect(opts.RelayGRPCAddress).To(Equal("localhost:9000"))
-	g.Expect(opts.RelayNumWorkers).To(Equal(5))
+	g.Expect(opts.Relay.Type).To(Equal("rabbit"))
+	g.Expect(opts.Relay.GRPCDisableTLS).To(BeTrue())
+	g.Expect(opts.Relay.GRPCTimeout).To(Equal(time.Second * 4))
+	g.Expect(opts.Relay.Token).To(Equal("8EDB98ED-0D85-4CFD-BE24-8B1E00A9F7C3"))
+	g.Expect(opts.Relay.GRPCAddress).To(Equal("localhost:9000"))
+	g.Expect(opts.Relay.NumWorkers).To(Equal(5))
 	g.Expect(opts.Rabbit.Exchange).To(Equal("testex"))
 	g.Expect(opts.Rabbit.Address).To(Equal("amqp://testing.tld:6379"))
 	g.Expect(opts.Rabbit.RoutingKey).To(Equal("testqueue"))
@@ -92,9 +92,9 @@ func TestHandleRabbitFlags_relay(t *testing.T) {
 	g.Expect(opts.Rabbit.ReadQueueDeclare).To(BeFalse())
 	g.Expect(opts.Rabbit.ReadAutoAck).To(BeFalse())
 	g.Expect(opts.Rabbit.ReadConsumerTag).To(Equal("plumber_123"))
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleRabbitFlags_write(t *testing.T) {
@@ -123,12 +123,12 @@ func TestHandleRabbitFlags_write(t *testing.T) {
 	g.Expect(opts.Rabbit.Address).To(Equal("amqp://testing.tld:6379"))
 	g.Expect(opts.Rabbit.RoutingKey).To(Equal("testqueue"))
 	g.Expect(opts.Rabbit.WriteAppID).To(Equal("plumber_123"))
-	g.Expect(opts.WriteInputData[0]).To(Equal("welovemessaging"))
-	g.Expect(opts.WriteInputFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputType).To(Equal("jsonpb"))
-	g.Expect(opts.WriteProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.WriteProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Write.InputData[0]).To(Equal("welovemessaging"))
+	g.Expect(opts.Write.InputFile).To(Equal("options.go"))
+	g.Expect(opts.Write.InputType).To(Equal("jsonpb"))
+	g.Expect(opts.Encoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Encoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleMQTTFlags_read(t *testing.T) {
@@ -163,9 +163,9 @@ func TestHandleMQTTFlags_read(t *testing.T) {
 	g.Expect(opts.MQTT.TLSCAFile).To(Equal("options.go"))
 	g.Expect(opts.MQTT.TLSClientKeyFile).To(Equal("options.go"))
 	g.Expect(opts.MQTT.TLSClientCertFile).To(Equal("options.go"))
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleMQTTFlags_write(t *testing.T) {
@@ -203,12 +203,12 @@ func TestHandleMQTTFlags_write(t *testing.T) {
 	g.Expect(opts.MQTT.TLSCAFile).To(Equal("options.go"))
 	g.Expect(opts.MQTT.TLSClientKeyFile).To(Equal("options.go"))
 	g.Expect(opts.MQTT.TLSClientCertFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputData[0]).To(Equal("welovemessaging"))
-	g.Expect(opts.WriteInputFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputType).To(Equal("jsonpb"))
-	g.Expect(opts.WriteProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.WriteProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Write.InputData[0]).To(Equal("welovemessaging"))
+	g.Expect(opts.Write.InputFile).To(Equal("options.go"))
+	g.Expect(opts.Write.InputType).To(Equal("jsonpb"))
+	g.Expect(opts.Encoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Encoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleKafkaFlags_lag(t *testing.T) {
@@ -256,10 +256,10 @@ func TestHandleKafkaFlags_read(t *testing.T) {
 	g.Expect(opts.Kafka.Topics).To(Equal([]string{"plumber_test"}))
 	g.Expect(opts.Kafka.GroupID).To(Equal("plumber_test_group"))
 	g.Expect(opts.Kafka.Timeout).To(Equal(time.Second * 3))
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
-	g.Expect(opts.ReadLag).To(BeTrue())
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Read.Lag).To(BeTrue())
 }
 
 func TestHandleKafkaFlags_write(t *testing.T) {
@@ -290,10 +290,10 @@ func TestHandleKafkaFlags_write(t *testing.T) {
 	g.Expect(opts.Kafka.Topics).To(Equal([]string{"plumber_test", "plumber_test_2"}))
 	g.Expect(opts.Kafka.WriteKey).To(Equal("plumber_test_key"))
 	g.Expect(opts.Kafka.Timeout).To(Equal(time.Second * 3))
-	g.Expect(opts.WriteInputData[0]).To(Equal("welovemessaging"))
-	g.Expect(opts.WriteInputFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputType).To(Equal("jsonpb"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Write.InputData[0]).To(Equal("welovemessaging"))
+	g.Expect(opts.Write.InputFile).To(Equal("options.go"))
+	g.Expect(opts.Write.InputType).To(Equal("jsonpb"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleAWSSQSFlags_read(t *testing.T) {
@@ -321,9 +321,9 @@ func TestHandleAWSSQSFlags_read(t *testing.T) {
 	g.Expect(opts.AWSSQS.RemoteAccountID).To(Equal("1234"))
 	g.Expect(opts.AWSSQS.ReadWaitTimeSeconds).To(Equal(int64(3)))
 	g.Expect(opts.AWSSQS.ReadReceiveRequestAttemptId).To(Equal("plumber_receiver"))
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleAWSSQSFlags_write(t *testing.T) {
@@ -352,10 +352,10 @@ func TestHandleAWSSQSFlags_write(t *testing.T) {
 	g.Expect(opts.AWSSQS.RemoteAccountID).To(Equal("1234"))
 	g.Expect(opts.AWSSQS.WriteDelaySeconds).To(Equal(int64(6)))
 	g.Expect(opts.AWSSQS.WriteAttributes).To(Equal(map[string]string{"tag": "value"}))
-	g.Expect(opts.WriteInputData[0]).To(Equal("welovemessaging"))
-	g.Expect(opts.WriteInputFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputType).To(Equal("jsonpb"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Write.InputData[0]).To(Equal("welovemessaging"))
+	g.Expect(opts.Write.InputFile).To(Equal("options.go"))
+	g.Expect(opts.Write.InputType).To(Equal("jsonpb"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 //--project-id=PROJECT-ID    Project id
@@ -382,9 +382,9 @@ func TestHandleGCPPubSubFlags_read(t *testing.T) {
 	g.Expect(opts.GCPPubSub.ReadSubscriptionId).To(Equal("plumber_sub"))
 	g.Expect(opts.GCPPubSub.ProjectId).To(Equal("plumber_project"))
 	g.Expect(opts.GCPPubSub.ReadAck).To(BeFalse())
-	g.Expect(opts.ReadProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
-	g.Expect(opts.ReadProtobufRootMessage).To(Equal("events.Message"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Decoding.ProtobufDirs).To(Equal([]string{"../test-assets/protos"}))
+	g.Expect(opts.Decoding.ProtobufRootMessage).To(Equal("events.Message"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
 
 func TestHandleGCPPubSubFlags_write(t *testing.T) {
@@ -409,8 +409,8 @@ func TestHandleGCPPubSubFlags_write(t *testing.T) {
 	g.Expect(cmd).To(Equal("write gcp-pubsub"))
 	g.Expect(opts.GCPPubSub.WriteTopicId).To(Equal("plumber_topic"))
 	g.Expect(opts.GCPPubSub.ProjectId).To(Equal("plumber_project"))
-	g.Expect(opts.WriteInputData[0]).To(Equal("welovemessaging"))
-	g.Expect(opts.WriteInputFile).To(Equal("options.go"))
-	g.Expect(opts.WriteInputType).To(Equal("jsonpb"))
-	g.Expect(opts.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
+	g.Expect(opts.Write.InputData[0]).To(Equal("welovemessaging"))
+	g.Expect(opts.Write.InputFile).To(Equal("options.go"))
+	g.Expect(opts.Write.InputType).To(Equal("jsonpb"))
+	g.Expect(opts.Decoding.AvroSchemaFile).To(Equal("../test-assets/avro/test.avsc"))
 }
