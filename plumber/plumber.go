6360ba4da7fa6493157616df57c71081def683a7
Mark Gregan <mark@batch.sh>
2021-12-17 15:31:49 -0500

    Splitting up GenerateMessageDescriptors() into smaller methods

--- plumber/plumber.go
+++ plumber/plumber.go
@@ -155,70 +155,83 @@ func (p *Plumber) Run() {
 }
 
 func GenerateMessageDescriptors(cliOpts *opts.CLIOptions) (map[pb.MDType]*desc.MessageDescriptor, error) {
-	descriptors := make(map[pb.MDType]*desc.MessageDescriptor)
-
 	if cliOpts.Read != nil && cliOpts.Read.DecodeOptions != nil {
-		if cliOpts.Read.DecodeOptions.DecodeType == encoding.DecodeType_DECODE_TYPE_PROTOBUF {
-			logrus.Debug("attempting to find decoding protobuf descriptors")
-
-			pbSettings := cliOpts.Read.DecodeOptions.ProtobufSettings
-			pbDirs := pbSettings.ProtobufDirs
-			pbRootMessage := pbSettings.ProtobufRootMessage
-
-			if err := validate.ProtobufOptionsForCLI(pbDirs, pbRootMessage); err != nil {
-				return nil, errors.Wrap(err, "unable to validate protobuf settings for decode")
-			}
-
-			md, err := pb.FindMessageDescriptor(pbDirs, pbRootMessage)
-			if err != nil {
-				return nil, errors.Wrap(err, "unable to find root message descriptor for decode")
-			}
-			descriptors[pb.MDEnvelope] = md
-
-			if pbSettings.ProtobufEnvelopeType == encoding.EnvelopeType_ENVELOPE_TYPE_SHALLOW {
-				md, err := pb.FindMessageDescriptor(pbDirs, pbSettings.ShallowEnvelopeMessage)
-				if err != nil {
-					return nil, errors.Wrap(err, "unable to find shallow envelope payload message descriptor for decode")
-				}
-				descriptors[pb.MDPayload] = md
-			}
-
-			return descriptors, nil
-		}
-
+		return generateMessageDescriptorsRead(cliOpts)
 	}
 
 	if cliOpts.Write != nil && cliOpts.Write.EncodeOptions != nil {
-		if cliOpts.Write != nil && cliOpts.Write.EncodeOptions.EncodeType == encoding.EncodeType_ENCODE_TYPE_JSONPB {
-			logrus.Debug("attempting to find encoding protobuf descriptors")
-
-			pbSettings := cliOpts.Write.EncodeOptions.ProtobufSettings
-			pbDirs := pbSettings.ProtobufDirs
-			pbRootMessage := pbSettings.ProtobufRootMessage
-
-			if err := validate.ProtobufOptionsForCLI(pbDirs, pbRootMessage); err != nil {
-				return nil, errors.Wrap(err, "unable to validate protobuf settings for encode")
-			}
-
-			md, err := pb.FindMessageDescriptor(pbDirs, pbRootMessage)
-			if err != nil {
-				return nil, errors.Wrap(err, "unable to find root message descriptor for encode")
-			}
-			descriptors[pb.MDEnvelope] = md
-
-			if pbSettings.ProtobufEnvelopeType == encoding.EnvelopeType_ENVELOPE_TYPE_SHALLOW {
-				md, err := pb.FindMessageDescriptor(pbDirs, pbSettings.ShallowEnvelopeMessage)
-				if err != nil {
-					return nil, errors.Wrap(err, "unable to find shallow envelope payload message descriptor for decode")
-				}
-				descriptors[pb.MDPayload] = md
-			}
-
-			return descriptors, nil
-		}
+		return generateMessageDescriptorsWrite(cliOpts)
 	}
 
 	return nil, nil
+}
+
+func generateMessageDescriptorsWrite(cliOpts *opts.CLIOptions) (map[pb.MDType]*desc.MessageDescriptor, error) {
+	if cliOpts.Write.EncodeOptions.EncodeType != encoding.EncodeType_ENCODE_TYPE_JSONPB {
+		return nil, nil
+	}
+
+	descriptors := make(map[pb.MDType]*desc.MessageDescriptor)
+
+	logrus.Debug("attempting to find encoding protobuf descriptors")
+
+	pbSettings := cliOpts.Write.EncodeOptions.ProtobufSettings
+	pbDirs := pbSettings.ProtobufDirs
+	pbRootMessage := pbSettings.ProtobufRootMessage
+
+	if err := validate.ProtobufOptionsForCLI(pbDirs, pbRootMessage); err != nil {
+		return nil, errors.Wrap(err, "unable to validate protobuf settings for encode")
+	}
+
+	envelopeMD, err := pb.FindMessageDescriptor(pbDirs, pbRootMessage)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to find root message descriptor for encode")
+	}
+	descriptors[pb.MDEnvelope] = envelopeMD
+
+	if pbSettings.ProtobufEnvelopeType == encoding.EnvelopeType_ENVELOPE_TYPE_SHALLOW {
+		payloadMD, err := pb.FindMessageDescriptor(pbDirs, pbSettings.ShallowEnvelopeMessage)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to find shallow envelope payload message descriptor for decode")
+		}
+		descriptors[pb.MDPayload] = payloadMD
+	}
+
+	return descriptors, nil
+}
+
+func generateMessageDescriptorsRead(cliOpts *opts.CLIOptions) (map[pb.MDType]*desc.MessageDescriptor, error) {
+	descriptors := make(map[pb.MDType]*desc.MessageDescriptor)
+
+	if cliOpts.Read.DecodeOptions.DecodeType != encoding.DecodeType_DECODE_TYPE_PROTOBUF {
+		return nil, nil
+	}
+
+	logrus.Debug("attempting to find decoding protobuf descriptors")
+
+	pbSettings := cliOpts.Read.DecodeOptions.ProtobufSettings
+	pbDirs := pbSettings.ProtobufDirs
+	pbRootMessage := pbSettings.ProtobufRootMessage
+
+	if err := validate.ProtobufOptionsForCLI(pbDirs, pbRootMessage); err != nil {
+		return nil, errors.Wrap(err, "unable to validate protobuf settings for decode")
+	}
+
+	md, err := pb.FindMessageDescriptor(pbDirs, pbRootMessage)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to find root message descriptor for decode")
+	}
+	descriptors[pb.MDEnvelope] = md
+
+	if pbSettings.ProtobufEnvelopeType == encoding.EnvelopeType_ENVELOPE_TYPE_SHALLOW {
+		payloadMD, err := pb.FindMessageDescriptor(pbDirs, pbSettings.ShallowEnvelopeMessage)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to find shallow envelope payload message descriptor for decode")
+		}
+		descriptors[pb.MDPayload] = payloadMD
+	}
+
+	return descriptors, nil
 }
 
 // validateConfig ensures all correct values for Config are passed
