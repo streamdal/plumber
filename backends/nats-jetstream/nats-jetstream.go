40a11ec0ae91d876f6fb95599c9b866045b47943
Daniel Selans <daniel.selans@gmail.com>
2022-06-21 17:25:12 -0700

    added pull consumer support for nats jetstream

--- backends/nats-jetstream/nats-jetstream.go
+++ backends/nats-jetstream/nats-jetstream.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"crypto/tls"
 	"net/url"
+	"time"
 
+	"github.com/batchcorp/plumber-schemas/build/go/protos/args"
 	"github.com/nats-io/nats.go"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -100,6 +102,106 @@ func (n *NatsJetstream) Close(_ context.Context) error {
 
 func (n *NatsJetstream) Test(_ context.Context) error {
 	return types.NotImplementedErr
+}
+
+func (n *NatsJetstream) validateExistingConsumerConfig(readArgs *args.NatsJetstreamReadArgs, config *nats.ConsumerConfig) error {
+	if readArgs == nil || config == nil {
+		return errors.New("readArgs and config cannot be nil")
+	}
+
+	if !readArgs.ExistingDurableConsumer {
+		return errors.New("expected existing durable consumer to be enabled - bug?")
+	}
+
+	// Consumer should be configured to use deliverByStartSeq
+	if readArgs.ConsumerStartSequence != 0 {
+		if config.DeliverPolicy != nats.DeliverByStartSequencePolicy {
+			return errors.New("existing consumer's deliver policy is not set to DeliverByStartSequence (tip: do not use existing consumer)")
+		}
+
+		return nil
+	}
+
+	if readArgs.ConsumerStartTime != "" {
+		if config.DeliverPolicy != nats.DeliverByStartTimePolicy {
+			return errors.New("existing consumer's deliver policy is not set to DeliverByStartTime (tip: do not use existing consumer)")
+		}
+	}
+
+	return nil
+}
+
+func (n *NatsJetstream) createConsumer(ctx nats.JetStreamContext, args *args.NatsJetstreamReadArgs) (*nats.ConsumerInfo, error) {
+	if args == nil || ctx == nil {
+		return nil, errors.New("both ctx and args cannot be nil")
+	}
+
+	if !args.CreateDurableConsumer && !args.ExistingDurableConsumer {
+		return nil, errors.New("durable consumer usage not enabled - nothing to do")
+	}
+
+	if args.ExistingDurableConsumer {
+		if args.ConsumerName == "" {
+			return nil, errors.New("consumer name must be specified when existing consumer is enabled")
+		}
+
+		consumerInfo, err := ctx.ConsumerInfo(args.Stream, args.ConsumerName)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to fetch existing consumer")
+		}
+
+		if err := n.validateExistingConsumerConfig(args, &consumerInfo.Config); err != nil {
+			return nil, errors.Wrap(err, "unable to validate existing consumer config")
+		}
+
+		return consumerInfo, nil
+	}
+
+	filterSubject := args.ConsumerFilterSubject
+
+	if filterSubject == "" {
+		filterSubject = args.Stream
+	}
+
+	consumerCfg := &nats.ConsumerConfig{
+		Durable:       getConsumerName(args.ConsumerName),
+		Description:   "plumber consumer",
+		OptStartSeq:   uint64(args.ConsumerStartSequence),
+		FilterSubject: filterSubject,
+		AckPolicy:     nats.AckExplicitPolicy,
+		DeliverPolicy: nats.DeliverLastPolicy,
+	}
+
+	// Which delivery policy should we use?
+	if args.ConsumerStartSequence != 0 {
+		consumerCfg.DeliverPolicy = nats.DeliverByStartSequencePolicy
+	} else if args.ConsumerStartTime != "" {
+		t, err := time.Parse(time.RFC3339, args.ConsumerStartTime)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to parse start time")
+		}
+
+		consumerCfg.DeliverPolicy = nats.DeliverByStartTimePolicy
+		consumerCfg.OptStartTime = &t
+	}
+
+	consumerInfo, err := ctx.AddConsumer(args.Stream, consumerCfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to add consumer")
+	}
+
+	n.log.Debugf("Created durable consumer '%s'", consumerInfo.Name)
+
+	return consumerInfo, nil
+}
+
+// If name is empty, generate a random'ish name; otherwise use provided
+func getConsumerName(name string) string {
+	if name == "" {
+		return "plumber-" + util.RandomString(8)
+	}
+
+	return name
 }
 
 func validateBaseConnOpts(connOpts *opts.ConnectionOptions) error {
